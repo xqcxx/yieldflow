cf6b19160bdad7a3079fdda41a6466daf78bc3c1
xqcxx <praisechinonso21@gmail.com>
2026-02-11 08:45:15 +0100

    feat(monitoring): Export all monitoring components - Part 18
    - Create components index.ts
    - Export all dashboard and visualization components
    - Enable clean imports for consumers
    
    Refs #23

---
+++ analytics/components/index.ts
@@ -0,0 +1,4 @@
+export { MonitoringDashboard } from './MonitoringDashboard';
+export { AlertNotification } from './AlertNotification';
+export { PerformanceDashboard } from './PerformanceDashboard';
+export { MetricsChart } from './MetricsChart';
